7c7874a94213ca8e0d77bae95c1908aa263e8310
prasad <prasad@prasad>
2022-09-13 22:45:51 +0530

    First Commit by Engineer 2 - removeFromCartTest

--- src/test/java/practice/AddToCartTest.java
+++ src/test/java/practice/AddToCartTest.java
@@ -1,5 +1,18 @@
 package practice;
 
+import org.testng.annotations.Test;
+
 public class AddToCartTest {
+	
+	@Test
+	public void addToCartTest()
+	{
+		System.out.println("Product Added");
+	}
+	@Test
+	public void removeFromCartTest()
+	{
+		System.out.println("product removed");
+	}
 
 }
